edb553febb1afbbb64b14f71ff20eb851c0a69eb
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2020-12-11 15:41:12 +0100

    [13.0][IMP] sale_by_packaging can be sold
    This change allows to be more specific on which product.packaging can be
    sold or not.
    By default the value from the product.packaging.type is used. But it is
    editable afterwards, with this change.

--- sale_by_packaging/models/product_packaging.py
+++ sale_by_packaging/models/product_packaging.py
@@ -1,14 +1,13 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProductPackaging(models.Model):
     _inherit = "product.packaging"
 
-    can_be_sold = fields.Boolean(
-        related="packaging_type_id.can_be_sold", readonly=True,
-    )
+    can_be_sold = fields.Boolean(string="Can be sold")
+
     force_sale_qty = fields.Boolean(
         string="Force sale quantity",
         help="Determine if during the creation of a sale order line, the "
@@ -18,3 +17,10 @@ class ProductPackaging(models.Model):
         "When the user will put 3 as quantity, the system can force the "
         "quantity to the superior unit (5 for this example).",
     )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        packagings = super().create(vals_list)
+        for pack in packagings:
+            pack.write({"can_be_sold": pack.packaging_type_id.can_be_sold})
+        return packagings
