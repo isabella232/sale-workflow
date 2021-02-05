39e2e05ca63f6b1c49e4caddcc7a6bded82f8538
jcoux <julien.coux@camptocamp.com>
2021-02-05 11:38:58 +0100

    fixup! [ADD] sale_coupon_most_expensive: init

--- sale_coupon_most_expensive/models/sale_order.py
+++ sale_coupon_most_expensive/models/sale_order.py
@@ -11,9 +11,14 @@ class SaleOrder(models.Model):
 
     def _get_most_expensive_line(self):
         # Mimic _get_cheapest_line method.
+        price_reduce_digits = self.env["decimal.precision"].precision_get(
+            self.order_line._fields["price_reduce"]._digits
+        )
         lines = self.order_line.filtered(
             lambda r: not r.is_reward_line
-            and float_compare(r.price_reduce, 0, precision_digits=2) > 0
+            and float_compare(
+                r.price_reduce, 0, precision_digits=price_reduce_digits
+            ) > 0
         )
         return max(lines, key=lambda r: r["price_reduce"])
 
