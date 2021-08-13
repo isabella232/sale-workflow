18e9088516105fbc6259e3e72c6e18c488c3a004
Matthieu Méquignon <matthieu.mequignon@camptocamp.com>
2021-08-13 11:02:50 +0200

    [13.0] Add sale_warehouse_calendar

---
+++ sale_warehouse_calendar/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Sale Warehouse Calendar",
+    "summary": "Computes sale order lead times with respect to to Warehouse calendar",
+    "version": "13.0.1.0.0",
+    "category": "Sales",
+    "website": "https://github.com/OCA/sale-workflow",
+    "author": "Camptocamp SA, Odoo Community Association (OCA)",
+    "maintainers": ["mmequignon"],
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["sale_cutoff_time_delivery", "stock_warehouse_calendar"],
+}
