1f1739b6ca73730ded335b26837f3ec28e46cd58
Sébastien Alix <sebastien.alix@camptocamp.com>
2022-03-30 17:47:12 +0200

    [FIX] sale_delivery_date: reset time on expected date
    Afterwards, if the customer has a delivery time window the expected date
    will be updated accordingly, or if there is none it'll be up to the
    carrier to determine the hour with the customer.

--- sale_delivery_date/models/sale_order_line.py
+++ sale_delivery_date/models/sale_order_line.py
@@ -245,6 +245,13 @@ class SaleOrderLine(models.Model):
             )
             # add back the security lead
             expected_date += td_security_lead
+            # This should be the delivery date.
+            # It is up to the carrier to determine the hour with the customer
+            # FIXME: reset time by respecting the TZ, this won't work with a
+            # negative time offset (UTC-5)
+            expected_date = expected_date.replace(
+                hour=0, minute=0, second=0, microsecond=0
+            )
         return expected_date
 
     def _delivery_window_expected_date(self, expected_date):
